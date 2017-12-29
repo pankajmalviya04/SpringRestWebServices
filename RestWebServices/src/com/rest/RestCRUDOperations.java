8a9fb01ab6e7d8687ab38fd67c13f66365f12179
pankajmalviya04 <pankajmalviya04@users.noreply.github.com>
2017-12-29 16:30:36 +0530

    Support of Json and XML format
    Support of Json and XML format

--- RestWebServices/src/com/rest/RestCRUDOperations.java
+++ RestWebServices/src/com/rest/RestCRUDOperations.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +21,19 @@ public class RestCRUDOperations {
 	@Autowired(required=true)
 	FetchData fetchData;
 	//Get call
-	@GetMapping(value="/all")
-	public List<Student> getAllSudent(){
-		
+		@GetMapping(value="/all")
+		public List<Student> getAllSudent(){		
+			List<Student> student = fetchData.getAllStudent();
+			return student;
+		}	
+	@GetMapping(value="/alljson", produces=MediaType.APPLICATION_JSON_VALUE)
+	public List<Student> getAllSudentInJsonFormat(){		
 		List<Student> student = fetchData.getAllStudent();
 		return student;
 	}
 	
-	//post call
-	@PostMapping(value="/all")
-	public List<Student> getAllSudentbyPost(){
+	@GetMapping(value="/allxml", produces=MediaType.APPLICATION_XML_VALUE)
+	public List<Student> getAllSudentInXmlFormat(){
 		
 		List<Student> student = fetchData.getAllStudent();
 		return student;
@@ -45,6 +48,13 @@ public class RestCRUDOperations {
 		List<Student> student = fetchData.addStudent(new Student(id, name, mobile));
 		return new ResponseEntity<List<Student>>(student, HttpStatus.CREATED);
 	}	
+	
+	//Put call with cosumes type is JSON, similar we can call for XML and other types
+	@PutMapping(value="/addJson", consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Boolean> addJsonStudent(){			
+			//List<Student> student = fetchData.addStudent(new Student(id, name, mobile));
+			return new ResponseEntity<Boolean>(true, HttpStatus.CREATED);
+		}	
 	
 	//delete Student
 	@DeleteMapping(value="/delete/{id}")
